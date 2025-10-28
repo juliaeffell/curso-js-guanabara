562d60bb7629891e7a8cac0d44a7e45957f492da
Mickael - notebook <knopmickael@gmail.com>
2025-10-27 23:33:03 -0300

    tipos de variaveis

--- aulaespecial/teste002-execucao.js
+++ aulaespecial/teste002-execucao.js
@@ -1,7 +1,33 @@
 // String
-let myfirstvar = 'pizza'
+let myfirstvar = 'pizza' 
 
 // Number
 let mysecondvar = 8
 
 // boolean
+let mythirdvar = true 
+
+// array
+let myfourth = ['tomate', 'cebola', 'cadeado', 6]
+
+// objeto
+let myfifth = { 
+  nome:'fulano de tal',
+  profissao: 'pedreiro',
+  salario: 1800,
+  habilidades: ['fazer piscina', 'tomar coquitcha', 'construir banheiro']
+}
+
+// array de objetos (json)
+let cities = [
+  {
+    nome: 'Tramandaí',
+    habitantes: 100000,
+    pib: 400000
+  },
+  {
+    nome: 'Torres',
+    habitantes: 200000,
+    pib: 650000
+  }
+]
---
+++ aulaespecial/teste002-prova.js
@@ -0,0 +1,13 @@
+// array de grupo de objetos carro
+var carros = [
+  {
+    modelo: 'tesla',
+    ano: 1987,
+    fabricante: 'tesla'
+  },
+  {
+    modelo: 'uno',
+    ano: 2010,
+    fabricante: 'ford'
+  }
+]
